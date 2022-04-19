204b06f31f1351e9cc3dc70ef086b623d4e626d7
Tamiris Shigaki <shigakiendo@gmail.com>
2022-04-19 01:52:13 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import FilterPlanets from './components/FilterPlanets';
+import TableStartWars from './components/TableStarWars';
 import AppProvider from './context/AppProvider';
-import TableStartWars from './TableStarWars';
 
 function App() {
   return (
     <AppProvider>
+      <FilterPlanets />
       <TableStartWars />
     </AppProvider>
   );
